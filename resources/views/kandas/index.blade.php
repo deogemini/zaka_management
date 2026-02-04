2ca57ed2bfe460e2aa94b14e83af6b7c8317abd8
Deo Gemini <geminideogratias8@gmail.com>
2026-02-04 22:43:15 +0300

    feat(views): add DataTables to kanda management page
    - Include DataTables CSS and JS via CDN with Bootstrap 5 styling
    - Initialize table with Swahili localization, custom pagination and sorting
    - Set default ordering by name column and disable sorting for action columns
    - Improve user experience with search, pagination controls and responsive design

--- resources/views/kandas/index.blade.php
+++ resources/views/kandas/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.admin')
 
+@push('styles')
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+@endpush
+
 @section('content')
 <h1 class="h3 mb-3"><strong>Kanda</strong> Management</h1>
 
@@ -18,7 +22,7 @@
                 <a href="{{ route('kandas.create') }}" class="btn btn-primary float-end mt-n4">Ongeza Kanda</a>
             </div>
             <div class="card-body">
-                <table class="table table-hover my-0">
+                <table id="kandaTable" class="table table-hover my-0 w-100">
                     <thead>
                         <tr>
                             <th>#</th>
@@ -56,3 +60,31 @@
     </div>
 </div>
 @endsection
+
+@push('scripts')
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#kandaTable').DataTable({
+            order: [[1, 'asc']],
+            pageLength: 50,
+            lengthMenu: [[10, 20, 50, 100, -1], [10, 20, 50, 100, 'All']],
+            language: {
+                search: 'Tafuta:',
+                lengthMenu: 'Onyesha _MENU_ rekodi',
+                info: 'Inaonyesha _START_ hadi _END_ ya _TOTAL_ rekodi',
+                paginate: { first: 'Kwanza', last: 'Mwisho', next: 'Ijayo', previous: 'Iliyopita' },
+                zeroRecords: 'Hakuna rekodi zilizopatikana',
+                infoEmpty: 'Hakuna rekodi',
+                infoFiltered: '(imuchujwa kutoka jumla ya rekodi _MAX_)'
+            },
+            columnDefs: [
+                { orderable: false, targets: [0, 5] }
+            ]
+        });
+    });
+});
+</script>
+@endpush
